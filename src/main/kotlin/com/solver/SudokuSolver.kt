b09a4bb967c280b81eaa9e4b054e189c903f24db
Kraus, Tomáš <krausto8@fel.cvut.cz>
2023-11-18 20:35:42 +0100

    implemented sudoku solver

--- src/main/kotlin/com/solver/SudokuSolver.kt
+++ src/main/kotlin/com/solver/SudokuSolver.kt
@@ -1,10 +1,10 @@
 package com.solver
 
 
-class SudokuSolver(val board: Array<Array<Int>>) {
+class SudokuSolver(private val board: Array<Array<Int>>) {
 
-    fun isValid(value: Int, x: Int, y: Int): Boolean {
-        for (i in 0..board.size) {
+    private fun isValid(value: Int, x: Int, y: Int): Boolean {
+        for (i in board.indices) {
             if (value == board[i][y] || value == board[x][i]) return false
         }
 
@@ -13,17 +13,29 @@ class SudokuSolver(val board: Array<Array<Int>>) {
 
         for (i in 0..2) {
             for (j in 0..2) {
-                if (value == board[segmentRowIndex*3 + i][segmentColumnIndex*3+j]) return false
+                if (value == board[segmentRowIndex * 3 + i][segmentColumnIndex * 3 + j]) return false
             }
         }
         return true
     }
 
-    fun solve(x: Int, y: Int) {
-//        if (x > 8 || y > 8) return
+    fun solve(x: Int = 0, y: Int = 0): Boolean {
+        if (x > 8) return true
+        else if (y > 8) return solve(x + 1, 0)
 
-        for (i in 1..9) {
-
+        if (board[x][y] == 0) {
+            for (i in 1..9) {
+                if (!isValid(i, x, y)) {
+                    continue
+                } else {
+                    board[x][y] = i
+                    if (solve(x, y + 1)) return true
+                    else board[x][y] = 0
+                }
+            }
+            return false
+        } else {
+            return solve(x, y + 1)
         }
     }
 
@@ -54,13 +66,14 @@ fun main() {
     )
 
     val board2 = arrayOf(
-        arrayOf(7,1,3),
-        arrayOf(9,0,2),
-        arrayOf(4,6,8)
+        arrayOf(7, 1, 3),
+        arrayOf(9, 0, 2),
+        arrayOf(4, 6, 8)
     )
 
     val ss = SudokuSolver(board)
     ss.printBoard()
-    println(ss.isValid(5, 1,1))
+    println(ss.solve())
+    ss.printBoard()
 
 }
